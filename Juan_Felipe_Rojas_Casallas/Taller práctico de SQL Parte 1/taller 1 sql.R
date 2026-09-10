25a87790113455cb551cca7c970f2a6a1410f645
EffixAICol <effixai.col@gmail.com>
2026-09-09 19:48:40 -0500

    Taller SQL

---
+++ Juan_Felipe_Rojas_Casallas/Taller práctico de SQL Parte 1/taller 1 sql.R
@@ -0,0 +1,9 @@
+url_chinook <- "https://raw.githubusercontent.com/lerocha/chinook-database/master/ChinookDatabase/DataSources/Chinook_Sqlite.sqlite"
+ruta_db     <- "chinook.db"
+
+if (!file.exists(ruta_db)) {
+  download.file(url_chinook, destfile = ruta_db, mode = "wb")
+  cat("Base de datos descargada en:", ruta_db, "\n")
+} else {
+  cat("La base de datos ya existe:", ruta_db, "\n")
+}
